2769470f3426605ed0f949dbe6e82c04f72df730
Davner <davner23@gmail.com>
2022-06-08 17:59:43 -0400

    Atualização - 08/06/2022

--- Teste/MeuTeste/src/ExpressõesMatematicas.java
+++ Teste/MeuTeste/src/ExpressõesMatematicas.java
@@ -1,9 +1,9 @@
 public class ExpressõesMatematicas {
     public static void main(String[] args) {
-        System.out.println("Resultado da multiplicação: " + Multiplicar(2, 3));   
+        System.out.println("Resultado da multiplicação: " + Multiplicar(2.34, 3.88));   
     }
 
-    public static int Multiplicar(int a, int b) {
-        return a * b / 3; 
+    public static float Multiplicar(double d, double e) {
+        return (float) (d * e / 3); 
     }
 }
